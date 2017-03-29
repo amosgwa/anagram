0e9fb3e60f38f1bbc2a29614591743e237143f1f
Amos Gwa <amosgwa@gmail.com>
2017-03-29 14:52:48 -0600

    Eveyrthing is working on the backend.

--- db.js
+++ db.js
@@ -1,29 +1,181 @@
-var mongoose = require('mongoose')
+var fs = require('fs')
+var sqlite3 = require('sqlite3').verbose();
 
-var uri = 'mongodb://localhost:27017/ibotta'
-var options = {
-  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 }}
+var sql_db; // Database is closed initially.
+
+function generateDatabase(file, res) {
+  // Open database to write and create.
+  // This resets the existing database.
+  sql_db = new sqlite3.Database('db.sql', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, () => {
+    sql_db.serialize(function () {
+      sql_db.run("DROP TABLE IF EXISTS anagram")
+      sql_db.run("CREATE TABLE anagram (keyword TEXT, word TEXT UNIQUE)", [], () => {
+        _generateDatabaseFromFile(file, res)
+      })
+    });
+  });
 }
 
-mongoose.connect(uri, options)
+// Function name starting with '_' are private.
+// Node's V8 engine fails without buffering because of large memory usage.
+// This makes sure the engine doesn't crash.
+function _generateDatabaseFromFile(file, res) {
+  var fileStream = fs.createReadStream(file, {
+    encoding: 'utf8'
+  });
+  console.log("Reading", file + "...")
+  var buffer = ''
+  // Stream the file.
+  fileStream.on('data', function (d) {
+    buffer += d.toString()
+    buffer = _processData(buffer)
+  })
+  // Database .close is a listener on the database.
+  // It will automatically be called after all execution.
+  fileStream.on('end', function () {
+    console.log("Done reading", file, ".")
+    console.log("Importing to database...")
+    sql_db.close((err, doc) => {
+      if (err) {
+        console.error(err)
+        res.status(500).send("Error importing database.")
+      }
+      else {
+        console.log("Finished imporrting. Database closed.")
+        res.status(201).send("Import successful.")
+      }
+    });
+  })
+}
 
-// Create a database (mongoose) instance.
-var db = mongoose.connection
+// Buffered data may include newline and fragment of a word.
+// The function makes sure to take care of the fragmented word.
+function _processData(buffer) {
+  var pos;
+  var words = []
+  // Keep going until a new line.
+  while ((pos = buffer.indexOf('\n')) >= 0) {
+    if (pos == 0) {
+      buf = buf.slice(1)
+      continue
+    }
+    words.push(buffer.slice(0, pos).trim())
+    buffer = buffer.slice(pos + 1)
+  }
+  // Add the list of words into the database.
+  _sql_db_add_words(words)
+  // Make sure to return updated buffer.
+  return buffer
+}
 
-// Connect to the database through mongoose.
-db.on('error', console.error.bind(console, 'Connection error:'))
-db.once('open', function(callback) {
-  console.log('db connected')
-})
+// The insertion will ignore errors if there is a dulplicate.
+function _sql_db_add_words(words) {
+  // Database is already opened by the parent's function call.
+  sql_db.parallelize(function () {
+    var stmt = sql_db.prepare("INSERT OR IGNORE INTO anagram VALUES (?,?)");
+    words.forEach(function (w) {
+      let sorted_word = w.split("").sort().join('').toLowerCase()
+      stmt.run(sorted_word, w)
+    })
+    stmt.finalize();
+  });
+}
 
-// Declare the Schema for the database.
-// We are storing sorted anagram word : to list of words
-// Example : 
-// ader : [ared, daer, dare, dear, read]
-var anagramSchema = mongoose.Schema({
-  keyword: String,
-  words: [String]
-})
+function getAnagrams(word, limit, res) {
+  var sorted_word = word.split('').sort().join('')
+  var words = []
 
-// Exports the model as an instance. 
-exports.Anagram = mongoose.model('Anagram', anagramSchema)
+  var query = `SELECT keyword, word FROM anagram WHERE keyword='${sorted_word}' AND word<>'${word}'`
+  // Make sure that the limit is accounted.
+  if (typeof limit != 'undefined') {
+    query += ` LIMIT ${limit}`
+  }
+  console.log(query)
+
+  // Open database.
+  sql_db = new sqlite3.Database('db.sql', sqlite3.OPEN_READWRITE)
+  // Append each row from result, then, the complete blocked is called to send with result.
+  sql_db.each(
+    query,
+    (err, row) => {
+      // Reading each row and append the result.
+      if (err) console.error(err)
+      else words.push(row.word)
+    }, (err, numRows) => {
+      // Completed.
+      if (err) {
+        console.error(err)
+        res.status(500).send("Database eror at getting anagrams for " + word)
+      } else {
+        var result = {
+          anagrams: words
+        }
+        console.log(result)
+        res.status(200).send(result)
+      }
+    });
+  sql_db.close()
+}
+
+function addAnagrams(words, res) {
+  // Open database.
+  sql_db = new sqlite3.Database('db.sql', sqlite3.OPEN_READWRITE)
+  // Run insertion of words in parallel.
+  sql_db.parallelize(function () {
+    var stmt = sql_db.prepare("INSERT OR IGNORE INTO anagram VALUES (?,?)");
+    words.forEach(function (w) {    
+      var sorted_word = w.split("").sort().join('').toLowerCase()
+      stmt.run(sorted_word, w)
+    })
+    stmt.finalize();
+  });
+  // Close database
+  sql_db.close((err, doc) => {
+    if (err) {
+      console.error("Error importing.")
+      res.status(500).send("Unable to add words.")
+    } else {
+      console.log("Finished imporrting. Database closed.")
+      res.status(201).send("Created")
+    }
+  });
+}
+
+function deleteWord(word, res) {
+  // Open database.
+  sql_db = new sqlite3.Database('db.sql', sqlite3.OPEN_READWRITE)
+  var query = `DELETE FROM anagram WHERE word='${word}'`
+  console.log("Delete ", query)
+  sql_db.run(query, [], (err, change) => {
+      if (err) {
+        console.error(err)
+        res.status(500).send("Unable to delete " + word)
+      } else {
+        console.log("Delete "+word+" : successful")
+        res.status(200).send("OK")
+      }
+    })
+  // Close database
+  sql_db.close()
+}
+
+// The insertion will ignore errors if there is a dulplicate.
+function deleteAllWords(res) {
+  sql_db = new sqlite3.Database('db.sql', sqlite3.OPEN_READWRITE)
+  var query = `DELETE FROM anagram`  
+  sql_db.run(query, [], (err, change) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send("Unable to delete all words")
+    } else {
+      console.log("All wrods are deleted successfully.")
+      res.status(204).send("No Content")
+    }
+  })
+}
+
+exports.generateDatabase = generateDatabase
+exports.getAnagrams = getAnagrams
+exports.addAnagrams = addAnagrams
+exports.deleteWord = deleteWord
+exports.deleteAllWords = deleteAllWords
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 // Imports
 var express = require('express')
-var app = express()
 var util = require('util');
-var sqlite3 = require('sqlite3').verbose();
-var sql_db = new sqlite3.Database('db.sql');  
+var bodyParser = require('body-parser')
 
-// var createTimer = require('unitimer')
-
-var fs = require('fs')
+var db = require('./db')
 var engines = require('consolidate')
 
-// Database instance.
-var Anagram = require('./db').Anagram
+var jsonParser = bodyParser.json()
+
+var app = express()
 
 // Set the default engine for rendering template.
 app.engine('hbs', engines.handlebars)
@@ -23,190 +20,53 @@ app.get('/', function (req, res) {
   res.render('index.hbs')
 })
 
-app.put('/generate', function (req, res) {
+// Generate database from dictionary.txt.
+// May take a long time based on the hard disk. Refer to server.timeout
+app.get('/generate', function (req, res) {
   // Create a collection.
   console.log("Clicked generate")
+  db.generateDatabase('dictionary.txt',res)
 })
 
-// var outStream = fs.createWriteStream('log.txt', {flags: 'a'})
-// var logStdout = process.stdout
+// This is a hack. Trying to make the 
+// curl -i -X POST -d '{ "words": ["read", "dear", "dare"] }' command work.
+app.use('/words.json',(req, res, next) => {
+  req.headers["content-type"] = "application/json"
+  next()
+})
 
-// console.log = function(d) { //
-//   outStream.write(util.format(d) + '\n');
-//   logStdout.write(util.format(d) + '\n');
-// };
-
-// console.error = console.log;
-
-// Reset the database.
-Anagram.remove({})
-  .then(_=> {
-    console.log("Database is reset.")
-    //generateDatabase()
-  })
-
-generateDatabase()
-
-function generateDatabase() {
-  //var timer = createTimer().start()
-  var fileStream = fs.createReadStream('dictionary.txt', {encoding: 'utf8'});
-  var buffer = ''
-
-  var prev = {}
-
-  console.log("Generating database from dictionary.txt")
-
-  var batch_bulkCommands = []
-  
-  create_table()
-  // Stream the file.
-  fileStream.on('data', function(d){
-    //console.log("Reading file...")
-    buffer += d.toString()
-    processData()
-  })
-  // Bulk write to the mongodb.
-  fileStream.on('end', function(){
-    console.log("Done Reading")
-    // sql_db.close();
-    // Promise.all(batch_bulkCommands)
-    //   .then(_=>{
-    //     console.log("Data successfully imported.")
-    //     console.log("Time :", timer.stop(), "ms")
-    //   })
-    //   .catch(err=>{
-    //     console.log(err)
-    //   })
-  })
-  
-  function processData() {
-    var pos;
-    var batch_command = []
-
-    var words = []
-
-    // Keep going until a new line.
-    while((pos = buffer.indexOf('\n')) >= 0) {
-      if(pos == 0) {
-        buf = buf.slice(1)
-        continue
-      }
-      //batch_command.push(processLine(buffer.slice(0, pos)))
-      words.push(buffer.slice(0, pos).trim())
-      buffer = buffer.slice(pos+1)
-    }
-    //ASK HANSEL : How do I execute only at the promise. Not here.
-    //batch_bulkCommands.push(Anagram.bulkWrite(batch_command))
-    // Anagram.bulkWrite(batch_command)
-    //   .then()
-    //   .catch(err=>{
-    //     console.log(err)
-    //     res.send("error")
-    //   })
-    sql_db_run(words)
-  }
-  // Create command for each word.
-  function processLine(line) {
-    let word = line.trim()
-    console.log(word)
-    // console.log("word " + word)
-    let sorted = word.split('').sort().join('')
-
-    var command = {}
-
-    if(prev[sorted]) {
-      command = {
-        updateOne: {
-          filter: {
-            keyword: sorted
-          },
-          update: {
-            $push: {
-              words: word
-            }
-          }  
-        }
-      }
-    } else {
-      command = {
-        insertOne: {
-          document: {
-            keyword: sorted,
-            words: [word]
-          }
-        }
-      } 
-      prev[sorted] = true
-    }
-
-    command = {
-        updateOne: {
-          filter: {
-            keyword: sorted
-          },
-          update: {
-            $push: {
-              words: word
-            }
-          },
-          upsert: true  
-        }
-      }
-
-    if(sorted == "aber" || sorted == "ader" || word == "Zyzzogeton") {
-      //console.log(word, prev[sorted])
-      //console.log(command)
-    }
-    return command
-  }
-}
-
-function create_table() {
-  sql_db.run("DROP TABLE IF EXISTS anagram")
-  sql_db.run("CREATE TABLE anagram (keyword TEXT, word TEXT)")
-}
-
-function sql_db_run(words) {
-  sql_db.parallelize(function() {
-    
-    var stmt = sql_db.prepare("INSERT INTO anagram VALUES (?,?)");
-
-    words.forEach(function(w){
-      stmt.run(w.split("").sort().join(''), w)
-    })
-    stmt.finalize();
-  });
-}
-
-function show_table() {
-  sql_db.each("SELECT rowid AS id, keyword, word FROM anagram", (err, row) => {
-      if(err) console.error(err)
-      else console.log(row.id, row.keyword, row.word);
-  });
-}
-
-// sql_db_run()
-
-
-app.get('/words', function(req, res){
+// Add the body of the words.json to the database.
+app.post('/words.json', jsonParser, function (req, res) {
   // Return the words.
-  Anagram.find({})
-    .then(recs=> res.send(recs))
-    .catch(err=> {
-      console.error(err) || res.send(err)
-    })
+  var words = req.body["words"]
+  console.log(words)
+  db.addAnagrams(words, res)
 })
 
-app.delete('/words', function(req, res){
-  Anagram.remove({})
-    .then(_=> res.send('Deletion Success'))
-    .catch(err=> res.send(err))
+// Remove all words from the database. NO TURNING BACK. :S
+app.delete('/words.json', (req,res)=>{
+  db.deleteAllWords(res)
 })
 
-function addAnagram(word){
-  console.log(word)
-}
+// Retrieve the anagrams of a given word.
+// The limit query from url can present or not.
+app.get('/anagrams/:word.json', (req, res)=>{
+  var word = req.params.word.trim()
+  var limit = req.query["limit"]
+  db.getAnagrams(word, limit, res)
+})
 
+// Delete a particuar word from the database.
+app.delete('/words/:word.json', (req,res)=>{
+  var word = req.params.word.trim()
+  db.deleteWord(word,res)
+})
+
+// Run the app on port 3000.
 var server = app.listen(3000, function () {
   console.log('Server running at http://localhost:' + server.address().port)
 })
+// This is required for generating the database from dictionary.txt.
+// The ingestion takes about 5~6 mins depending on the type of hard-drive.
+// The default is 2 mins, and the browser will retry after the set timout.
+server.timeout = 600000
